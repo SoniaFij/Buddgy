f17f27cded507ef4d37e6dd5be3496b7b139478e
SoniaFij <94309386+SoniaFij@users.noreply.github.com>
2024-07-06 18:39:30 +0200

    Add files via upload

---
+++ Models/BudgetTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetTracker.Models
+{
+    public class BudgetTrackerDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public BudgetTrackerDbContext(DbContextOptions<BudgetTrackerDbContext> options)
+            : base(options)
+        {
+            
+
+        }
+    }
+}
